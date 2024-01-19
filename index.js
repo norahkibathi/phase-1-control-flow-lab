8e5ab30472d0e6fb242ed6dff012d522de19e306
norahkibathi <norah.njeri@student.moringaschool.com>
2024-01-19 04:10:03 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-function scuberGreetingForFeet(ride){ 
-  if (ride <= 400) {
-    return  "This one is on me!" ;
-  
-    }
-  else if (ride > 2000 && ride <= 2500){
-    return "I will gladly take your thirty bucks."
+function scuberGreetingForFeet(feet){
+  // Write your code here!
+  if(feet <= 400){
+    return 'This one is on me!';
   }
-  else if (ride > 2500) {
-    return "No can do"
+  else if(feet > 2000 && feet <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (feet > 2500) {
+    return 'No can do.';
   }
 }
 
 function ternaryCheckCity(city){
-  return city == 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  if (city==='NYC'){
+    return 'Ok, sounds good.' 
+  }
+  else{
+      return 'No go.';
+     }
 }
-
 function switchOnCharmFromTip(tip){
   switch (tip) {
-    case "generousTip":
-    console.log('Thank you so much.')
-    case "not generousTip":
-      console.log("Thank you ")
-      case "anything else ":
-        console.log("Bye")
+    case 'generous':
+        return 'Thank you so much.';
+    case 'not as generous':
+        return 'Thank you.';
+    default:
+        return 'Bye.';
   }
- 
 }
